6d88db5039c3ff9a4ade55f50af6054168f82bdf
Alexey Belov <alexey.belov@nordigy.ru>
2018-10-23 19:27:15 +0300

    add middleware and reducers

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,4 +1,4 @@
-import { getInitalData } from '../utils/api';
+import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receiveTweets } from '../actions/tweets';
 import { setAuthedUser } from '../actions/authedUser';
@@ -6,9 +6,9 @@ import { setAuthedUser } from '../actions/authedUser';
 const AUTHED_ID = 'tylermcginnis';
 export function handleInitialData() {
   return dispatch => {
-    return getInitalData().then(({ users, tweets }) => {
+    return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
-      dispatch(receiveTweets(receiveTweets));
+      dispatch(receiveTweets(tweets));
       dispatch(setAuthedUser(AUTHED_ID));
     });
   };
