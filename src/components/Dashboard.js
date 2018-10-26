2001f96a256f80751c1add2631633f9de3bf681b
Alexey Belov <alexey.belov@nordigy.ru>
2018-10-26 19:04:53 +0300

    add icons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Tweet from './Tweet';
 
 class Dashboard extends Component {
   render() {
@@ -9,7 +10,7 @@ class Dashboard extends Component {
         <ul className="dashboard-list">
           {this.props.tweetIds.map(id => (
             <li key={id}>
-              <div>TWEET ID: {id}</div>
+              <Tweet id={id} />
             </li>
           ))}
         </ul>
@@ -19,7 +20,6 @@ class Dashboard extends Component {
 }
 
 function mapStateToProps({ tweets }) {
-  console.log(tweets);
   return {
     tweetIds: Object.keys(tweets).sort((a, b) => tweets[b].timestamp - tweets[a].timestamp)
   };
